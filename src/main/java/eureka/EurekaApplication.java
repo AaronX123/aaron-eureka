7be4bb54013b91787772814e8c1d91ccd276e55f
xiaoyouming <739243573@qq.com>
2020-04-15 14:16:27 +0800

    (+) fix

---
+++ src/main/java/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xiaoyouming
+ * @version 1.0
+ * @since 2020-04-15
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
